357917bf01054658d1bccdd84210ca218a596792
Galileo <Galileo-dev@users.noreply.github.com>
2021-01-17 00:32:21 +0000

    items

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,91 @@
 <script>
-  import Header from "./components/header.svelte";
+  import Header from "./components/Header.svelte";
+  import Item from "./components/item.svelte";
+  let Accesories = "assets/Desktop-Accessories.jpg";
+  let ModelS = "assets/Desktop-ModelS.jpeg";
+  let Model3 = "assets/Desktop-Model3.jpeg";
+  let ModelX = "assets/Desktop-ModelX.jpeg";
+  let ModelY = "assets/Desktop-ModelY.jpeg";
+  let SolarPanels = "assets/Desktop-SolarPanels.jpeg";
+  let SolarRoof = "assets/Desktop-SolarRoof.jpeg";
 </script>
 
 <main>
   <!-- {HEADER} -->
   <Header />
-  <!-- {items=>Snapping} -->
-  <!-- {items=>Snapping} -->
-  <!-- {items=>Snapping} -->
-  <!-- {items=>Snapping} -->
+  <div class="app__itemsContainer">
+    <Item
+      title="Lowest Cost Solar Panels in America"
+      desc="Money-back guarantee"
+      backgroundImg={SolarPanels}
+      leftBtnTxt="ORDER NOW"
+      leftBtnLink=""
+      rightBtnTxt="LEARN MORE"
+      rightBtnLink=""
+      twoButtons="true"
+      first
+    />
+    <Item
+      title="Model S"
+      desc="Starting at $69,420"
+      backgroundImg={ModelS}
+      leftBtnTxt="CUSTOM ORDER"
+      leftBtnLink=""
+      rightBtnTxt="EXISTING INVENTORY"
+      rightBtnLink=""
+      twoButtons="true"
+    />
+    <Item
+      title="Model 3"
+      desc="Order Online for Touchless Delivery"
+      backgroundImg={Model3}
+      leftBtnTxt="CUSTOM ORDER"
+      leftBtnLink=""
+      rightBtnTxt="EXISTING INVENTORY"
+      rightBtnLink=""
+      twoButtons="true"
+    />
+    <Item
+      title="Model X"
+      desc="Order Online for Touchless Delivery"
+      backgroundImg={ModelX}
+      leftBtnTxt="CUSTOM ORDER"
+      leftBtnLink=""
+      rightBtnTxt="EXISTING INVENTORY"
+      rightBtnLink=""
+      twoButtons="true"
+    />
+    <Item
+      title="Model Y"
+      desc="Order Online for Touchless Delivery"
+      backgroundImg={ModelY}
+      leftBtnTxt="CUSTOM ORDER"
+      leftBtnLink=""
+      rightBtnTxt="LEARN MORE"
+      rightBtnLink=""
+      twoButtons="true"
+    />
+    <Item
+      title="Solar for New Roofs"
+      desc="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+      backgroundImg={SolarRoof}
+      leftBtnTxt="ORDER NOW"
+      leftBtnLink=""
+      rightBtnTxt="LEARN MORE"
+      rightBtnLink=""
+      twoButtons="true"
+    />
+    <Item
+      title="Accessories"
+      desc=""
+      backgroundImg={Accesories}
+      leftBtnTxt="SHOP"
+      leftBtnLink=""
+      rightBtnTxt="LEARN MORE"
+      rightBtnLink=""
+      twoButtons="false"
+    />
+  </div>
 </main>
 
 <style>
---
+++ src/components/Item.svelte
@@ -0,0 +1,87 @@
+<script>
+  import Button from "./button.svelte";
+  import ExpandDown from "svelte-material-icons/ChevronDown.svelte";
+
+  export let title,
+    desc,
+    backgroundImg,
+    leftBtnTxt,
+    leftBtnLink,
+    rightBtnTxt,
+    rightBtnLink,
+    first,
+    twoButtons;
+</script>
+
+<main class="item" style="background-image: url('{backgroundImg}')">
+  <div class="item__container">
+    <div class="item__text">
+      <p>{title}</p>
+      <div class="item__textDesc">
+        <p>{desc}</p>
+      </div>
+    </div>
+    <div class="item_lowerThird">
+      <div class="item__buttons" />
+      <Button imp="primary" text="{leftBtnTxt}," link={leftBtnLink} />
+      {#if twoButtons}
+        <Button imp="secondary" text="{rightBtnTxt}," link={rightBtnLink} />
+      {/if}
+    </div>
+    {#if first}
+      <div class="item__expand">
+        <ExpandDown size="1.5em" />
+      </div>
+    {/if}
+  </div>
+</main>
+
+<style>
+  .item {
+    height: 100vh;
+    width: 100vw;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+  }
+
+  .item__container {
+    width: 40vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .item__text {
+    margin-top: 20%;
+    display: flex;
+    flex-direction: column;
+    font-size: 34px;
+    color: #393c41;
+    align-items: center;
+    flex: 1;
+  }
+  .item__textDesc {
+    font-weight: 200;
+    font-size: 14px;
+  }
+
+  .item_lowerThird {
+    margin-bottom: 5%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .item__buttons {
+    display: flex;
+  }
+
+  .item__expand {
+    color: white;
+    /* width: 100%; */
+    margin-top: 3%;
+    justify-content: center;
+  }
+</style>
